67555bce7bd5ec467cdebfc32848bbe087f6c8e4
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-07-26 11:40:09 +0200

    sync code

--- raster/src/main.cpp
+++ raster/src/main.cpp
@@ -1,12 +1,33 @@
 #include <mn/IO.h>
 #include <mn/Memory.h>
 #include <mn/Buf.h>
+#include <mn/Pool.h>
 
+#define NOMINMAX
 #include <Windows.h>
 
 #include <stdint.h>
 #include <assert.h>
+#include <chrono>
 
+// clamp
+inline static int
+min(int a, int b)
+{
+	return b < a ? b : a;
+}
+
+inline static int
+max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+inline static int
+clamp(int x, int minimum, int maximum)
+{
+	return max(minimum, min(x, maximum));
+}
 
 // Pixel
 struct Pixel
@@ -87,7 +108,7 @@ image_clear(Image& self)
 
 
 
-// Math
+// Vec2i
 struct Vec2i
 {
 	int x, y;
@@ -173,6 +194,183 @@ operator/=(Vec2i& a, Vec2i b)
 	return a;
 }
 
+inline static int
+lensqr(Vec2i a)
+{
+	return a.x * a.x + a.y * a.y;
+}
+
+inline static int
+len(Vec2i a)
+{
+	sqrt(lensqr(a));
+}
+
+inline static Vec2i
+min(Vec2i a, Vec2i b)
+{
+	return Vec2i{
+		a.x < b.x ? a.x : b.x,
+		a.y < b.y ? a.y : b.y
+	};
+}
+
+inline static Vec2i
+max(Vec2i a, Vec2i b)
+{
+	return Vec2i{
+		a.x > b.x ? a.x : b.x,
+		a.y > b.y ? a.y : b.y
+	};
+}
+
+
+// AABB Box
+struct Box
+{
+	Vec2i min, max;
+};
+
+inline static int
+box_width(const Box& self)
+{
+	return self.max.x - self.min.x;
+}
+
+inline static int
+box_height(const Box& self)
+{
+	return self.max.y - self.min.y;
+}
+
+inline static bool
+box_smaller(const Box &a, const Box& b)
+{
+	return lensqr(a.max - a.min) < lensqr(b.max - b.min);
+}
+
+inline static bool
+box_intersect(const Box& a, const Box& b)
+{
+	bool x_overlap = (
+		(a.min.x >= b.min.x && a.min.x < b.min.x + box_width(b)) ||
+		(b.min.x >= a.min.x && b.min.x < a.min.x + box_width(a))
+	);
+
+	bool y_overlap = (
+		(a.min.y >= b.min.y && a.min.y < b.min.y + box_height(b)) ||
+		(b.min.y >= a.min.y && b.min.y < a.min.y + box_height(a))
+	);
+
+	return x_overlap && y_overlap;
+}
+
+
+//Shapes
+struct Shape
+{
+	virtual Box bbox() = 0;
+	virtual int distance(Vec2i pixel) = 0;
+	virtual Pixel sample(Vec2i pixel) = 0;
+};
+
+struct Circle final: Shape
+{
+	Box box;
+	Pixel color;
+	Vec2i center;
+	int r;
+
+	Box bbox() override {
+		return box;
+	}
+
+	int distance(Vec2i p) override {
+		return lensqr(p -center) - r * r;
+	}
+
+	Pixel sample(Vec2i p) {
+		if (distance(p) < 0)
+			return color;
+		return Pixel{};
+	}
+};
+
+
+// Quadtree
+struct Quadnode
+{
+	Box box;
+	Quadnode* top_left;
+	Quadnode* top_right;
+	Quadnode* bottom_left;
+	Quadnode* bottom_right;
+};
+
+struct Quadtree
+{
+	mn::Pool node_pool;
+	Quadnode* root;
+	uint32_t limit;
+};
+
+inline static Quadnode*
+quadtree_node_new(Quadtree& self)
+{
+	Quadnode* res = (Quadnode*)mn::pool_get(self.node_pool);
+	*res = Quadnode{};
+	return res;
+}
+
+inline static void
+quadtree_split(Quadtree& self, Quadnode* node)
+{
+	if(box_width(node->box) < self.limit && box_height(node->box) < self.limit) {
+		return;
+	}
+	int w2 = box_width(node->box) / 2;
+	int h2 = box_height(node->box) / 2;
+
+	node->top_left = quadtree_node_new(self);
+	node->top_left->box.min = node->box.min;
+	node->top_left->box.max = node->top_left->box.min + Vec2i{ w2, h2 };
+
+	node->top_right = quadtree_node_new(self);
+	node->top_right->box.min = node->box.min + Vec2i{ w2, 0 };
+	node->top_right->box.max = node->top_right->box.min + Vec2i{ w2, h2 };
+
+	node->bottom_left = quadtree_node_new(self);
+	node->bottom_left->box.min = node->box.min + Vec2i{ 0, h2 };
+	node->bottom_left->box.max = node->bottom_left->box.min + Vec2i{ w2, h2 };
+
+	node->bottom_right = quadtree_node_new(self);
+	node->bottom_right->box.min = node->box.min + Vec2i{ w2, h2 };
+	node->bottom_right->box.max = node->bottom_right->box.min + Vec2i{ w2, h2 };
+
+	quadtree_split(self, node->top_left);
+	quadtree_split(self, node->top_right);
+	quadtree_split(self, node->bottom_left);
+	quadtree_split(self, node->bottom_right);
+}
+
+inline static Quadtree
+quadtree_new(uint32_t width, uint32_t height, uint32_t limit)
+{
+	Quadtree self{};
+	self.node_pool = mn::pool_new(sizeof(Quadnode), 1024);
+	self.root = quadtree_node_new(self);
+	self.root->box.max = Vec2i{int(width), int(height)};
+	self.limit = limit;
+	quadtree_split(self, self.root);
+	return self;
+}
+
+inline static void
+quadtree_free(Quadtree& self)
+{
+	mn::pool_free(self.node_pool);
+}
+
 
 
 // Swapchain
@@ -231,14 +429,20 @@ swapchain_resize(Swapchain& self, uint32_t width, uint32_t height)
 typedef struct IEngine* Engine;
 struct IEngine
 {
+	int width;
+	int height;
 	Swapchain chain;
+	Quadtree tree;
 };
 
 inline static Engine
 engine_new(uint32_t width, uint32_t height)
 {
 	Engine self = mn::alloc<IEngine>();
+	self->width = int(width);
+	self->height = int(height);
 	self->chain = swapchain_new(width, height);
+	self->tree = quadtree_new(width, height, width / 10);
 	return self;
 }
 
@@ -246,6 +450,7 @@ inline static void
 engine_free(Engine self)
 {
 	swapchain_free(self->chain);
+	quadtree_free(self->tree);
 	mn::free(self);
 }
 
@@ -261,13 +466,33 @@ engine_swap(Engine self)
 inline static void
 engine_resize(Engine self, uint32_t width, uint32_t height)
 {
+	self->width = width;
+	self->height = height;
 	swapchain_resize(self->chain, width, height);
+	quadtree_free(self->tree);
+	self->tree = quadtree_new(width, height, width / 2);
 }
 
 inline static void
-engine_circle(Engine self, uint32_t x, uint32_t y, uint32_t r)
+engine_circle(Engine self, int x, int y, int r, Pixel color)
 {
+	x += self->width / 2;
+	y += self->height / 2;
 
+	Circle shape{};
+	shape.box.min = Vec2i{ clamp(x - r, 0, self->width), clamp(y - r, 0, self->height) };
+	shape.box.max = Vec2i{ clamp(x + r, 0, self->width), clamp(y + r, 0, self->height) };
+	shape.color = color;
+	shape.center = Vec2i{ x, y };
+	shape.r = r;
+
+	Box b = shape.bbox();
+
+	Image img = swapchain_back(self->chain);
+
+	for (int j = b.min.y; j < b.max.y; ++j)
+		for (int i = b.min.x; i < b.max.x; ++i)
+			img(i, j) += shape.sample(Vec2i{ i, j });
 }
 
 
@@ -338,10 +563,18 @@ window_proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		if(self)
 			engine_resize(self->engine, LOWORD(lParam), HIWORD(lParam));
 		break;
-	case WM_PAINT:
+	case WM_PAINT: {
+		auto start = std::chrono::high_resolution_clock::now();
 		self->render(self->engine);
+		auto end = std::chrono::high_resolution_clock::now();
+		mn::printfmt("render: {:10}us, ", std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+
+		start = std::chrono::high_resolution_clock::now();
 		window_blit(self, engine_swap(self->engine));
+		end = std::chrono::high_resolution_clock::now();
+		mn::printfmt("blit: {:10}us\n", std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
 		break;
+	}
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
@@ -419,14 +652,31 @@ window_poll(Window self)
 inline static void
 app_render(Engine self)
 {
-	static int mov = 0;
+	static int anim_r = 50;
+	static int anim_rf = 1;
 
-	Image img = swapchain_back(self->chain);
-	for (size_t j = 0; j < img.height; ++j)
-		for (size_t i = 0; i < img.width; ++i)
-			img(i, j) += Pixel{ (i + mov) % 255, (j + mov) % 255, 0, 255 };
+	static int anim_x = -self->width/2;
+	static int anim_xf = 1;
 
-	++mov;
+	static int anim_y = -self->height/2;
+	static int anim_yf = 1;
+
+	engine_circle(self, anim_x, 0, anim_r, Pixel{ 255 });
+	engine_circle(self, 0, 0, 50, Pixel{ 0, 255 });
+	engine_circle(self, 100, anim_y, anim_r, Pixel{ 0, 0, 255 });
+
+	anim_r += anim_rf;
+	anim_x += anim_xf;
+	anim_y += anim_yf;
+
+	if (anim_r > 500 || anim_r < 50)
+		anim_rf *= -1;
+
+	if (anim_x > self->width / 2 || anim_x < -self->width / 2)
+		anim_xf *= -1;
+
+	if (anim_y > self->height/ 2 || anim_y < -self->height / 2)
+		anim_yf *= -1;
 }
 
 int main(int argc, char** argv)
